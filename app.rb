ee6cd4abe06c9a078f2cf4686300197f7c4ebfee
Student <student@Makerss-MacBook-Pro.local>
2019-03-18 18:03:15 +0000

    Inserted forms to index

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
+get '/random_cat' do
+  @name = %w[Amigo Oscar Viking].sample
+  erb(:index)
+end
 
-get '/cat' do
-  @cat = ["Amigo", "Oscar", "Viking"].sample
+get '/named_cat' do
+  @name = params[:name]
   erb(:index)
 end
